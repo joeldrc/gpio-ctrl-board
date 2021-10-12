df88f4ffa69b783b129df207159c2fa361419b75
joeldrc <33722197+joeldrc@users.noreply.github.com>
2021-10-12 11:28:02 +0200

    add test script

---
+++ test script/test.py
@@ -0,0 +1,26 @@
+import time
+import requests
+
+
+def queryValues(url):
+    response = requests.get(url)
+    val = response.content
+    val = val.decode("utf-8")
+    print("\r" + val, end = '')
+    return val
+
+
+def port(val):
+    for i in range(256):        
+        queryValues('http://btms-mcu-0.cern.ch/portVal='+ val + str(i) + '&')
+        time.sleep(0.01)
+ 
+
+print("Ethernet remote control v1 \nTerminal reading \n")
+while (True):
+    port('a')
+    port('b')
+    port('c')
+    port('d')
+
+    
