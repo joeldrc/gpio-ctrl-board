eee5f0f401774ce77f39bff2f1f149714e2f6dc2
joeldrc <33722197+joeldrc@users.noreply.github.com>
2021-10-13 16:38:20 +0200

    code updated

--- test script/CTRL attenuator.py
+++ test script/CTRL attenuator.py
@@ -32,80 +32,82 @@ def updatePort(txt):
     print(txt + ' -> setted')
       
 
-print("Ethernet remote control v1 \n*** JD 10/2021 ***\n> CTRL attenuator <\n")
+print("Ethernet remote control v1 \n--- JD 10/2021 ---\n>>> CTRL attenuator <<<\n")
 print("CMD list: \n- <on-off> \n- <a/b><0-6> [dB steps] \n- read [read settings]\n")
 
 while (True):
     cmd = input('>>> ')
+    cmd = cmd.split(' ')
+    
+    for i in range(len(cmd)):
+       
+        if(cmd[i] == 'on'):       
+            portA |= 0b00000010
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'off'):
+            portA &= 0b11111101
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'a0'):
+            portA &= 0b10101010
+            portB &= 0b01011111
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'a1'):
+            portA |= 0b00000001
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'a2'):
+            portA |= 0b00000100
+            updatePort(cmd[i])
 
-    if(cmd == 'on'):       
-        portA |= 0b00000010
-        updatePort(cmd)
-        
-    elif(cmd == 'off'):
-        portA &= 0b11111101
-        updatePort(cmd)
-        
-    elif(cmd == 'a0'):
-        portA &= 0b10101010
-        portB &= 0b01011111
-        updatePort(cmd)
-        
-    elif(cmd == 'a1'):
-        portA |= 0b00000001
-        updatePort(cmd)
-        
-    elif(cmd == 'a2'):
-        portA |= 0b00000100
-        updatePort(cmd)
+        elif(cmd[i] == 'a3'):
+            portA |= 0b00010000
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'a4'):
+            portA |= 0b01000000
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'a5'):
+            portB |= 0b10000000
+            updatePort(cmd[i])
 
-    elif(cmd == 'a3'):
-        portA |= 0b00010000
-        updatePort(cmd)
-        
-    elif(cmd == 'a4'):
-        portA |= 0b01000000
-        updatePort(cmd)
-        
-    elif(cmd == 'a5'):
-        portB |= 0b10000000
-        updatePort(cmd)
+        elif(cmd[i] == 'a6'):
+            portB |= 0b00100000
+            updatePort(cmd[i])
 
-    elif(cmd == 'a6'):
-        portB |= 0b00100000
-        updatePort(cmd)
+        elif(cmd[i] == 'b0'):
+            portB &= 0b10100000
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'b1'):
+            portB |= 0b00000001
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'b2'):
+            portB |= 0b00001000
+            updatePort(cmd[i])
 
-    elif(cmd == 'b0'):
-        portB &= 0b10100000
-        updatePort(cmd)
-        
-    elif(cmd == 'b1'):
-        portB |= 0b00000001
-        updatePort(cmd)
-        
-    elif(cmd == 'b2'):
-        portB |= 0b00001000
-        updatePort(cmd)
+        elif(cmd[i] == 'b3'):
+            portB |= 0b00000100
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'b4'):
+            portB |= 0b00000010
+            updatePort(cmd[i])
+            
+        elif(cmd[i] == 'b5'):
+            portB |= 0b00010000
+            updatePort(cmd[i])
 
-    elif(cmd == 'b3'):
-        portB |= 0b00000100
-        updatePort(cmd)
-        
-    elif(cmd == 'b4'):
-        portB |= 0b00000010
-        updatePort(cmd)
-        
-    elif(cmd == 'b5'):
-        portB |= 0b00010000
-        updatePort(cmd)
+        elif(cmd[i] == 'b6'):
+            portB |= 0b01000000
+            updatePort(cmd[i])
 
-    elif(cmd == 'b6'):
-        portB |= 0b01000000
-        updatePort(cmd)
-
-    elif(cmd == 'read'):      
-        print(*queryValues(boardUrl + '/getValues'), sep = '\n')
-
+        elif(cmd[i] == 'read'):      
+            print(*queryValues(boardUrl + '/getValues'), sep = '\n')
     
      
 
